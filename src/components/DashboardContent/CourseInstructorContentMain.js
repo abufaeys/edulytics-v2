28a2c23d425d1d1c743f61a732297d3ac969bcbc
caffeinatedshots <theonlyrt6@gmail.com>
2018-04-04 13:06:43 +0800

    Change search bar to include CourseInstructor and CohortAdmin

--- src/components/DashboardContent/CourseInstructorContentMain.js
+++ src/components/DashboardContent/CourseInstructorContentMain.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Grid, Statistic, Card, Loader } from 'semantic-ui-react'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
+import StudentHistogram from './StudentVisuals/StudentHistogram';
+import StudentAverageTime from './StudentVisuals/AverageTime';
+import StudentLevelsCompleted from './StudentVisuals/LevelsCompleted';
+import AssignmentTracker from './StudentVisuals/AssignmentTracker/AssignmentTracker';
 /*
   This is the main template for each of the dashboard we will do.
   This file is only to be changed if:
@@ -9,27 +15,63 @@ import { Grid } from 'semantic-ui-react'
   For more info on Grids, link is here: https://react.semantic-ui.com/collections/grid#grid-example-divided-number
 */
 
-const CourseInstructorContentMain = () => (
-  <Grid columns={3} doubling stackable>
-      <Grid.Column>
-        <h1> This is a grid </h1>
-      </Grid.Column>
-      <Grid.Column>
-        <h1> This is a grid </h1>
-      </Grid.Column>
-      <Grid.Column>
-        <h1> This is a grid </h1>
-      </Grid.Column>
-      <Grid.Column>
-        <h1> This is a grid </h1>
-      </Grid.Column>
-      <Grid.Column>
-        <h1> This is a grid </h1>
-      </Grid.Column>
-      <Grid.Column>
-        <h1> This is a grid </h1>
-      </Grid.Column>
-  </Grid>
-)
+class CourseInstructorContentMain extends Component {
 
-export default CourseInstructorContentMain
+  render(){
+
+    return (
+      <div style={{"backgroundColor": "#F2F2F2"}}>
+        <Grid columns={3} doubling as={Card.Group} >
+          <Card>  
+            <Statistic label='Elo Rating' value='5,550' />
+          </Card>
+          <Card>
+            {this.props.fetchChartsDatabaseStatus === "FETCHED" ?
+              <StudentAverageTime chartsDatabase={this.props.chartsDatabase} userId={this.props.userId} /> :
+              <Loader active inline='centered'/>
+            }
+          </Card>
+          <Card>
+            {this.props.fetchChartsDatabaseStatus === "FETCHED" ?
+              <StudentLevelsCompleted chartsDatabase={this.props.chartsDatabase} userId={this.props.userId} /> :
+              <Loader active inline='centered'/>
+            }
+          </Card>           
+        </Grid>
+        <Grid columns={2} doubling stackable as={Card.Group} >
+          <Card>
+            <StudentHistogram chartsDatabase={this.props.fetchChartsDatabaseStatus === "FETCHED" ? this.props.chartsDatabase : {}}/>
+          </Card>
+          <Card>
+            {this.props.fetchChartsDatabaseStatus === "FETCHED" ?
+              <AssignmentTracker chartsDatabase={this.props.chartsDatabase} userId={this.props.userId} /> :
+              <Loader active inline='centered'/>
+            }
+          </Card>
+          <Grid.Column>
+            <Card
+              header={this.props.fetchChartsDatabaseStatus === "FETCHED" ? this.props.userNames[this.props.userId] : "loading"}
+              meta='Friend'
+              description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            />
+          </Grid.Column>
+        </Grid>
+      </div>
+      )
+  }
+  }
+
+const mapStateToProps = state => ({
+  userNames:state.firebase.staticDatabase.UserNames,
+  fetchChartsDatabaseStatus: state.firebase.fetchChartsDatabaseStatus,
+  chartsDatabase: state.firebase.chartsDatabase,
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+
+}, dispatch)
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(CourseInstructorContentMain)
