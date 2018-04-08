f93755feb6fc783d3398f0ac574e3996450f2764
gnosis-agora <gnosis-agora@hotmail.com>
2018-04-08 21:44:02 +0800

    Add course average RadarChart on student dashboard

--- src/components/DashboardContent/StudentVisuals/SkillsRadarChart/PentagonChart.js
+++ src/components/DashboardContent/StudentVisuals/SkillsRadarChart/PentagonChart.js
@@ -6,12 +6,13 @@ import {
 	PolarAngleAxis, 
 	PolarRadiusAxis,
 	ResponsiveContainer,
-	Tooltip
+	Tooltip,
+	Legend
 } from 'recharts';
 
 
 
-const PentagonChart = ({levelStatus, completedness, relativeAvgtime, proactiveness, effort, diligence, studentName}) => {
+const PentagonChart = ({levelStatus, completedness, relativeAvgtime, proactiveness, effort, diligence, average, studentName}) => {
 	const data = [
 	    { subject: 'Hardworking', A: Math.round(levelStatus*100)},
 	    { subject: 'Dedication', A: Math.round(completedness*100)},
@@ -21,14 +22,20 @@ const PentagonChart = ({levelStatus, completedness, relativeAvgtime, proactivene
 	    { subject: 'Diligence', A: Math.round(diligence*100)},
 	];	
 
+	for (let i=0;i<average.length;i++) {
+		data[i]['B'] = average[i];
+	}
+
 	return (
 		<ResponsiveContainer width="100%" height={500}>
 	  	<RadarChart outerRadius={150} data={data}>
 	      <Radar name={studentName} dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
+	      <Radar name="Course Average" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6}/>
 	      <PolarGrid />
 	      <Tooltip/>
 	      <PolarAngleAxis dataKey="subject" />
 	      <PolarRadiusAxis angle={60} domain={[0, 100]}/>
+	      <Legend />
 	    </RadarChart>
 	  </ResponsiveContainer>
   );	
