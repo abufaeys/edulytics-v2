030c334093a905c637f7f3be7dba14bb503d2c36
caffeinatedshots <theonlyrt6@gmail.com>
2018-04-19 18:28:19 +0800

    Update weak student monitoring dashboard

--- src/components/DashboardContent/CourseInstructorVisuals/WeakStudentMonitoring/WeakStudentMonitoringCard.js
+++ src/components/DashboardContent/CourseInstructorVisuals/WeakStudentMonitoring/WeakStudentMonitoringCard.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
 import { ResponsiveContainer } from 'recharts';
+import { getEloRating } from '../../../../constants/helpers.js';
 
 import SkillsRadarChartMin from './SkillsRadarChartMin';
 
@@ -15,14 +16,51 @@ import DataNotFound from '../../DataNotFound';
 */	
 
 class WeakStudentMonitoringCard extends Component{
+	studentElo(){
+		var attributes = [];
+		attributes.push(this.props.chartsDatabase.Student.studentlevelStatus.data[this.props.userId] * 100);
+		attributes.push(this.props.chartsDatabase.Student.assignmentCompletedness.data[this.props.userId] * 100);
+		attributes.push(this.props.chartsDatabase.Student.relativeAvgtime.data[this.props.userId]["self"] * 100);
+		attributes.push(this.props.chartsDatabase.Student.studentProactiveness.data[this.props.userId] * 100);
+		attributes.push(this.props.chartsDatabase.Student.normalizedTotalplaytime.data[this.props.userId]["self"] * 100);
+		attributes.push(this.props.chartsDatabase.Student.dilligence.data[this.props.userId]);
+		return getEloRating(attributes);
+	}
+
+	averageTimeTaken(){
+		return Math.round(this.props.chartsDatabase.Student.averagePlaytime.data[this.props.userId])
+	}
+
+	levelsCompleted(){
+		return this.props.chartsDatabase.Student.levelComplete.data[this.props.userId]
+	}
+
+	assignmentsDue(){
+		var data = this.props.chartsDatabase.Student.assignmentsDue[this.props.userId];
+		var assignments = []
+		for (let course in data) {
+			for (let assignment in data[course]) {
+				if (data[course][assignment] !== false) {
+					assignments.push(data[course][assignment]);
+				}
+			}
+		}
+		return assignments.length
+	}
 	
 	render(){
 		try{
 				return (
+				<div>
 				<ResponsiveContainer width="100%">
 					<SkillsRadarChartMin chartsDatabase={this.props.chartsDatabase} staticDatabase={this.props.staticDatabase} userId={this.props.userId} 
                   userName={this.props.userNames[this.props.userId].name}/>
                   </ResponsiveContainer>
+                  <Statistic label="Elo Rating" value={this.studentElo()} horizontal />
+                  <Statistic label="Levels Completed" value={this.levelsCompleted()} horizontal />
+                  <Statistic label="Average Time Taken" value={this.averageTimeTaken()} horizontal />
+                  <Statistic label="Assignments Due" value={this.assignmentsDue()} horizontal />
+                  </div>
 				)
 		}
 		catch(e){
